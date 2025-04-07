a80126ad987ee1c55fd149c0d62638717dd8b6cb
pebbsmom <perezteri@hotmail.com>
2025-04-07 18:20:35 -0400

    updated starter code issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,10 @@ import { authenticateToken } from './utils/auth.js';
 
 import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -26,10 +30,10 @@ const startApolloServer = async () => {
 
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
 
     app.get('*', (_req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
   
